8c3c95a7e30be8813aed6afd8498ec5e2dd457e2
Ralph Jarrod Gorham <35537756+TheGuyInApt42@users.noreply.github.com>
2024-11-12 07:35:43 -0500

    comment out api keys in login component

--- src/lib/components/login/Login.svelte
+++ src/lib/components/login/Login.svelte
@@ -3,7 +3,7 @@
   import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
   import '@shopify/shopify-api/adapters/node';
 
-  const shopify = shopifyApi({
+  /* const shopify = shopifyApi({
     apiKey: '620f7beb1ae387d8d6ab2f9b413c28db',
     apiSecretKey: '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',
     scopes: ['read_products', 'unauthenticated_read_customers'],
@@ -14,7 +14,7 @@
     //apiVersion: ApiVersion.July22,
     hostName: 'localhost:5175',
     adminApiAccessToken: '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'
-  });
+  }); */
 
   const handleSubmit = async (event) => {
     event.preventDefault();
