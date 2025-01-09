d420373bfba61f01cfa63f4a66eeb3a9c330b470
Ralph Jarrod Gorham <35537756+TheGuyInApt42@users.noreply.github.com>
2025-01-08 19:11:02 -0500

    staging test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -117,7 +117,7 @@
   </section>
 
   <h1 class="text-center">Play N Trade Camp Hill</h1>
-  <!-- <NewsletterSignup /> -->
+  <NewsletterSignup />
 
   <div class="flex w-full justify-center px-4 sm:px-6 md:px-8">
     <div
